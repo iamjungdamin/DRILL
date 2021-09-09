49b59cd816b7b85fac0e343c74bb80bd2544bd47
iamjungdamin <jdm0366@naver.com>
2021-09-09 20:26:44 +0900

    DRILL 03 완성
    2020184032 정다민

---
+++ 03/name.py
@@ -0,0 +1,80 @@
+import turtle
+
+# 정
+
+turtle.forward(100)
+turtle.backward(50)
+turtle.right(130)
+turtle.forward(80)
+turtle.backward(80)
+turtle.left(80)
+turtle.forward(80)
+
+turtle.penup()
+turtle.goto(100, -40)
+turtle.pendown()
+turtle.left(50)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.right(180)
+turtle.forward(80)
+
+turtle.penup()
+turtle.goto(50, -100)
+turtle.pendown()
+turtle.circle(30)
+
+# 다
+
+turtle.penup()
+turtle.goto(200, -10)
+turtle.left(90)
+turtle.forward(80)
+turtle.pendown()
+turtle.left(180)
+turtle.forward(80)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(80)
+
+turtle.penup()
+turtle.goto(300, 0)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(120)
+turtle.backward(60)
+turtle.left(90)
+turtle.forward(40)
+
+# 민
+
+turtle.penup()
+turtle.goto(360, 0)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+turtle.forward(80)
+turtle.left(90)
+turtle.forward(80)
+turtle.left(90)
+turtle.forward(80)
+
+turtle.penup()
+turtle.goto(460, 10)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(100)
+
+turtle.penup()
+turtle.goto(400, -100)
+turtle.pendown()
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(70)
+
+
+
+turtle.exitonclick()
