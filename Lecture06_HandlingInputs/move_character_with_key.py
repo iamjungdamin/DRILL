52f422d4c672f1e969142dd16cba6f6e94ceedcb
iamjungdamin <jdm0366@naver.com>
2021-09-30 18:21:01 +0900

    09/30 수업 추가

--- Lecture06_HandlingInputs/move_character_with_key.py
+++ Lecture06_HandlingInputs/move_character_with_key.py
@@ -4,6 +4,7 @@ from pico2d import *
 def handle_events():
     global running
     global dir
+    global state
     events = get_events()
 
     for event in events:
@@ -12,9 +13,11 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
+                state = 1
             elif event.key == SDLK_LEFT:
                 dir -= 1
-            elif event.key ==SDLK_ESCAPE:
+                state = 0
+            elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
@@ -30,12 +33,13 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
-dir = 0  # -1 left, +1 right
+dir = 0     # -1 left, +1 right
+state = 1   # 0 left, 1 right
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * state, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
